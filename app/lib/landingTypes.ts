17731061afd44ce90d3e47e16d538861f7c2929d
donmatt773 <donmatt773@gmail.com>
2026-08-26 09:44:24 +0800

    bug fixes

--- app/lib/landingTypes.ts
+++ app/lib/landingTypes.ts
@@ -1,3 +1,13 @@
+export type RoomBedSummary = {
+  name: string;
+  quantity: number;
+};
+
+export type RoomImage = {
+  fileUrl: string;
+  altText?: string;
+};
+
 export type FeaturedRoom = {
   _id: string;
   name: string;
@@ -5,8 +15,14 @@ export type FeaturedRoom = {
   description?: string;
   maxGuests: number;
   nightlyRate: number;
+  halfDayRate?: number;
+  wholeDayRate?: number;
   primaryImage?: string;
   primaryImageAlt?: string;
+  images?: RoomImage[];
+  beds?: RoomBedSummary[];
+  features?: string[];
+  amenities?: string[];
 };
 
 export type FeaturedPromo = {
